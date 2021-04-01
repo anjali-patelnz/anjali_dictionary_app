e2295384102198aae30a04004a70553e4e1af40f
anjali-patelnz <73802454+anjali-patelnz@users.noreply.github.com>
2021-04-01 14:55:28 +1300

    Photo api call created and photo gallery is displayed

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,17 +8,29 @@ import "./Dictionary.css";
 export default function Dictionary() {
   const [word, setWord] = useState(" ");
   let [searchResults, setSearchResults] = useState(null);
+  let [gallery, setGallery] = useState(null);
 
-  function handleApiCall(response) {
+  function handleDictionaryApiCall(response) {
     setSearchResults(response.data[0]);
+  }
+
+  function handlePexelsApiCall(response) {
+    setGallery(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${word}`;
-    axios.get(apiUrl).then(handleApiCall);
-  }
+    axios.get(apiUrl).then(handleDictionaryApiCall);
 
-  //563492ad6f917000010000014f9f9c360fcd45a78509cb88a592277c
+    let pexelsApiKey =
+      "563492ad6f917000010000014f9f9c360fcd45a78509cb88a592277c";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=4`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsApiCall);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -49,7 +61,7 @@ export default function Dictionary() {
         </div>
       </section>
       <SearchResults data={searchResults} />
-      <PhotoSearch />
+      <PhotoSearch data={gallery} />
     </div>
   );
 }
--- src/PhotoSearch.js
+++ src/PhotoSearch.js
@@ -1,5 +1,23 @@
 import React from "react";
 
-export default function PhotoSearch() {
-  return <p>Hello from PhotoSearch</p>;
+export default function PhotoSearch(props) {
+  if (props.data) {
+    return (
+      <div className="PhotoSearch container">
+        <div className="row">
+          {props.data.map(function (photo, index) {
+            return (
+              <div className="col" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.landscape} className="img-fluid" />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
