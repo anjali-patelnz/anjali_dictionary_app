d260c97277d6770fb969226e435e498b205e142b
anjali-patelnz <73802454+anjali-patelnz@users.noreply.github.com>
2021-04-06 12:29:18 +1200

    Updated all links to open in new tab

--- src/App.js
+++ src/App.js
@@ -11,29 +11,56 @@ function App() {
             <div className="footnote">
               <p className="text-center">
                 This site was coded by{" "}
-                <a href="https://focused-johnson-3a2ad6.netlify.app/">
+                <a
+                  href="https://focused-johnson-3a2ad6.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   Anjali Patel
                 </a>{" "}
                 and is open-sourced on{" "}
-                <a href="https://github.com/anjali-patelnz/anjali_dictionary_app">
+                <a
+                  href="https://github.com/anjali-patelnz/anjali_dictionary_app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   GitHub
                 </a>
               </p>
             </div>
             <div className="social-links d-flex justify-content-evenly">
-              <a href="https://focused-johnson-3a2ad6.netlify.app/">
+              <a
+                href="https://focused-johnson-3a2ad6.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <i className="fas fa-user-alt"></i>
               </a>
-              <a href="https://www.linkedin.com/in/anjalipatelnz/">
+              <a
+                href="https://www.linkedin.com/in/anjalipatelnz/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <i className="fab fa-linkedin"></i>
               </a>
-              <a href="https://github.com/anjali-patelnz">
+              <a
+                href="https://github.com/anjali-patelnz"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <i className="fab fa-github-square"></i>
               </a>
             </div>
             <div className="credits">
               <p>
-                Photos provided by <a href="https://www.pexels.com">Pexels</a>
+                Photos provided by{" "}
+                <a
+                  href="https://www.pexels.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Pexels
+                </a>
               </p>
             </div>
           </div>
